52e29b127a261068589f47e536521db747be2a22
secondary-smiles <shavkinder@gmail.com>
2022-12-21 23:34:46 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Apod from "./lib/apis/nasa/Apod.svelte";
+</script>
+
+<main>
+    <div id="message">
+
+        <h1>Aw shit</h1>
+        <h2>this was supposed to be a christmas present but I guess I got carried away..</h2>
+        <h3>happy birthday too? (this isn't your birthday present though)</h3>
+        <h4>I guess, here's a few cool things (I'm making this in only one night, remember)</h4>
+    </div>
+    <Apod></Apod>
+    <div id="footer-stuff">
+        <h5>LOVE U SIMON</h5>
+    </div>
+</main>
---
+++ src/lib/apis/nasa/Apod.svelte
@@ -0,0 +1,23 @@
+<script>
+    const APOD_KEY = "k2uZJunhObVqwkESabbPhCKTk5FZ5zrcfqX3lV6V";
+
+    class nasaImage {
+        constructor(title, description, imageUrl) {
+            this.title = title;
+            this.description = description;
+            this.imageUrl = imageUrl;
+        }
+    }
+
+    async function getNasaData() {
+        const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=" + APOD_KEY);
+        const data = await response.json();
+        console.log(data)
+    }
+
+    getNasaData();
+</script>
+
+<main>
+    <img src="" id="nasa-apod" alt="NASA Picture of the Day">
+</main>
